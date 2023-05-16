411dedff3d7d508937417a93cac947c5a1620bde
Nima <nima@Nimas-MacBook-Air.local>
2023-05-16 21:27:15 +0330

    Initial implementation of order app models

--- shop/orders/models.py
+++ shop/orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from ..productions.models import Product
+
 
 # Create your models here.
+
+
+class Address(models.Model):
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+
+
+class Order(models.Model):
+    address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()

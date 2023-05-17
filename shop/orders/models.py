963382165999728bce136a4adc9721485bdbf241
Nima <nima@Nimas-MacBook-Air.local>
2023-05-17 14:09:38 +0330

    add user_id in Address and Order model

--- shop/orders/models.py
+++ shop/orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from productions.models import Product
-from core.models import BaseModel
+from core.models import BaseModel, User
 
 # Create your models here.
 
@@ -9,11 +9,13 @@ class Address(models.Model):
     state = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     street = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL)
 
 
 class Order(BaseModel):
     address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
     paid = models.BooleanField(default=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Order {self.id}'

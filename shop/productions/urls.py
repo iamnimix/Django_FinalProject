5294b5f7bbb59333642e78eca0a2463ed4356292
Nima <nima@Nimas-MacBook-Air.local>
2023-05-20 18:04:18 +0330

    add ProductList view also with category filter

--- shop/productions/urls.py
+++ shop/productions/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'productions'
 
 urlpatterns = [
-    path('', views.main_page),
+    path('', views.ProductList.as_view(), name='product_list'),
+    path("<slug:category_slug>/", views.ProductList.as_view(), name="product_list_by_category"),
     path('<int:id>/', views.detail_page, name='detail'),
 ]
--- shop/productions/views.py
+++ shop/productions/views.py
@@ -1,12 +1,28 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from .models import Product, Image
+from django.views.generic import ListView
+
+from .models import Product, Image, Category
 
 
 # Create your views here.
 
-def main_page(request):
-    products = Product.objects.all()
-    return render(request, 'list.html', context={'products': products})
+class ProductList(ListView):
+    model = Product
+    template_name = 'list.html'
+    context_object_name = 'products'
+    paginate_by = 4
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_slug = self.kwargs.get('category_slug')
+
+        if category_slug:
+            # Filter products by category
+            category = Category.objects.get(slug=category_slug)
+            queryset = queryset.filter(category=category)
+
+        return queryset
 
 
 def detail_page(request, id):

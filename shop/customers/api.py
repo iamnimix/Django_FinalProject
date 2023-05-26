3b5eb5974d1dfbb6bc06c742ca982230b9c5df0e
Nima <nima@Nimas-MacBook-Air.local>
2023-05-26 12:42:41 +0330

    add login api

--- shop/customers/api.py
+++ shop/customers/api.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import logout, authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import UserSerializer
@@ -18,3 +22,24 @@ class RegisterApi(APIView):
         else:
             errors = form.errors
             return Response(errors)
+
+
+class LoginAPIView(APIView):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        if request.POST.get("logout"):
+            logout(request)
+            return redirect("login")
+        phone = request.POST['phone']
+        password = request.POST['password']
+
+        # Validate the user's credentials
+        user = authenticate(phone=phone, password=password)
+        if user is None:
+            # If the credentials are not valid, return an error response
+            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            login(request, user)
+            return redirect("productions:landing_page")
--- shop/customers/urls.py
+++ shop/customers/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'customers'
 
 urlpatterns = [
+      path('api/login/', api.LoginAPIView.as_view(), name='login'),
       path('register/', views.RegisterView.as_view(), name="sign_up"),
       path('api/register/', api.RegisterApi.as_view(), name="register"),
 ]

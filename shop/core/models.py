338a8eb58bd2dcb25aab4af33cb40fd7b0832253
Nima <nima@Nimas-MacBook-Air.local>
2023-05-26 12:17:45 +0330

    -

--- shop/core/models.py
+++ shop/core/models.py
@@ -4,28 +4,6 @@ import jdatetime
 
 # Create your models here.
 
-class JalaliDateField(models.DateField):
-    def from_db_value(self, value, expression, connection):
-        if value is None:
-            return value
-        if isinstance(value, jdate):
-            return value.togregorian().date()
-        return value
-
-    def to_python(self, value):
-        if value is None:
-            return value
-        if isinstance(value, jdate):
-            return value.togregorian().date()
-        return value
-
-    def get_prep_value(self, value):
-        if value is None:
-            return value
-        if isinstance(value, jdatetime):
-            return value.date()
-        return value
-
 
 class BaseModel(models.Model):
     class Meta:
